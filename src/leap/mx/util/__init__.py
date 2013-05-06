685ff8562ccb8553875624ea944a272dc59f4fa2
Isis Lovecruft <isis@torproject.org>
2013-05-06 18:06:31 +0000

    Fix import statement in module initilisation util.__init__.py.

--- src/leap/mx/util/__init__.py
+++ src/leap/mx/util/__init__.py
@@ -8,5 +8,7 @@ Module intialization file for leap.mx.util.
 import version
 version = version.Version()
 
+from . import config, log, net, storage, version
+
 __all__ = ['config', 'log', 'net', 'storage', 'version']
 
